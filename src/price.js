943860d98779ef64e7f0d9fdf5cc64a9c4bdd2dc
SeanRichhh <sean-richards@outlook.com>
2022-03-29 18:00:07 +0100

    extension not completed

--- src/price.js
+++ src/price.js
@@ -18,7 +18,37 @@ class Price {
         for (let i = 0; i < basket.length; i++) {
             totalPrice += basket[i].price
         }
+        return totalPrice
+    }
+
+    getBasketTotalMessage(basket){
+        const totalPrice = this.getBasketTotal(basket)
+
         return `The total price of the items in your basket is £${Number(totalPrice.toFixed(2))}`
+    }
+
+    getReceipt(){
+       const totalPrice = this.getBasketTotal(basket)
+       
+        let date = new Date()
+
+        // Start with an empty string        
+        let receipt = ""
+
+
+        // Receipt heading
+        receipt += "===============\n"
+        //   Use date where needed
+        receipt += ` Date: ${date}\n`
+        // Receipt details
+        //   Use basket contents to create receipt for each type of bagel
+        // Receipt total
+        receipt += " totalPrice: ${totalPrice}\n"
+        // Receipt footer
+        //   Use date where needed
+
+        return receipt
+
     }
 
 }
